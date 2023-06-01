7b2908ef3f89926e4a93f6ebfed0e601152b9bb5
BlackDiamond <caden.feller@gmail.com>
2023-05-31 22:05:11 -0700

    started work on inventory grid

--- src/components/TurtleInv.tsx
+++ src/components/TurtleInv.tsx
@@ -1,17 +1,21 @@
 import * as React from "react";
 import Turtle from '../UI'
-import Grid from "@mui/material/Grid"
+import Grid from '@mui/material/Grid';
+import Item from "@mui/material/ListItem";
 export default class TurtleInv extends React.Component<Turtle, {}> {
 constructor (props: Turtle){
   super(props);
 }
 
+
+
 render() {
+
   return (  
     <div>
       <h1>Turtle Inventory</h1>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
+      <Grid>
+        <Grid>
           <Item>xs=8</Item>
         </Grid>
         <Grid item xs={4}>
