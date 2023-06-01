ee49cc0b18cae539b6800e86ad09ed5325438956
Caden Feller <caden.feller@gmail.com>
2023-06-01 13:36:23 -0700

    added styling for control pannel

--- src/components/TurtleInv.tsx
+++ src/components/TurtleInv.tsx
@@ -2,32 +2,70 @@ import * as React from "react";
 import Turtle from '../UI'
 import Grid from '@mui/material/Grid';
 import Item from "@mui/material/ListItem";
+import { UIStyle } from "../styles/UIStyle"
 export default class TurtleInv extends React.Component<Turtle, {}> {
 constructor (props: Turtle){
   super(props);
 }
 
-
-
 render() {
-
+  let styles = new UIStyle();
+  let gridStyle = styles.GridStyle;
+  let itemStyle = styles.ItemStyle;
   return (  
     <div>
       <h1>Turtle Inventory</h1>
-      <Grid>
-        <Grid>
-          <Item>xs=8</Item>
+      <Grid container style={gridStyle} id="top row">
+        <Grid item style={itemStyle}>
+          <Item>1</Item>
         </Grid>
-        <Grid item xs={4}>
-          <Item>xs=4</Item>
+        <Grid item style={itemStyle}>
+          <Item>2</Item>
         </Grid>
-        <Grid item xs={4}>
-          <Item>xs=4</Item>
+        <Grid item style={itemStyle}>
+          <Item>3</Item>
         </Grid>
-        <Grid item xs={8}>
-          <Item>xs=8</Item>
+        <Grid item style={itemStyle}>
+          <Item>4</Item>
+        </Grid>
+        <Grid item style={itemStyle}>
+          <Item>5</Item>
+        </Grid>
+        <Grid item style={itemStyle}>
+          <Item>6</Item>
+        </Grid>
+        <Grid item style={itemStyle}>
+          <Item>7</Item>
+        </Grid>
+        <Grid item style={itemStyle}>
+          <Item>8</Item>
+        </Grid>
+        <Grid item style={itemStyle}>
+          <Item>9</Item>
+        </Grid>
+        <Grid item style={itemStyle}>
+          <Item>10</Item>
+        </Grid>
+        <Grid item style={itemStyle}>
+          <Item>11</Item>
+        </Grid>
+        <Grid item style={itemStyle}>
+          <Item>12</Item>
+        </Grid>
+        <Grid item style={itemStyle}>
+          <Item>13</Item>
+        </Grid>
+        <Grid item style={itemStyle}>
+          <Item>14</Item>
+        </Grid>
+        <Grid item style={itemStyle}>
+          <Item>15</Item>
+        </Grid>
+        <Grid item style={itemStyle}>
+          <Item>16</Item>
         </Grid>
       </Grid>
+
     </div>
     );
   }
--- src/styles/UIStyle.ts
+++ src/styles/UIStyle.ts
@@ -1,7 +1,29 @@
-import {createGlobalStyle} from "styled-components"
+export class UIStyle {
 
-export default createGlobalStyle`
-    *{
-        border: 5px solid red;
-    }
-`
+    public GridStyle = {
+        padding: 10,
+        width: 420,
+        height: 420,
+        xs: 12,
+		top: 100,
+		left: 0,
+		zIndex: 10,
+		overflow: 'hidden',
+        spacing: 5
+    };
+
+    public ItemStyle = {
+        border: "5px solid green",
+        borderRadius: "15px",
+        opacity: 0.25,
+        padding: 10,
+        background: '#252525',
+        height: 100,
+        width: 100,
+
+        fontSize: 20,
+        xs: 12,
+        spacing: 10
+    };
+
+}
