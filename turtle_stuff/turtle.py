05c62949bafe38b1cad20e96531255dd5e66534b
Caden Feller <caden.feller@gmail.com>
2024-02-28 15:11:45 -0800

    started adding naming convention

--- turtle_stuff/turtle.py
+++ turtle_stuff/turtle.py
@@ -1,22 +1,28 @@
 # sets the message that will indicate a disconnection 
 DISCONNECT_MESSAGE = "END-OF-LINE"
 
+"""
+Turtle naming convention:
+{type}-{number}-{revision}
+
+
+"""
 class Turtle:
     
     def __init__(self, websocket):
         self.websocket = websocket
-        
-        
-    def main(self) :
+
+
+    def main(self):
         msg = self.recv()
         print(msg)
         if msg == DISCONNECT_MESSAGE:
             return False
         self.send("return turtle.turnRight()")
         return True
-        
+
     async def send(self, message):
         await self.websocket.send(message)
-        
+
     async def recv(self):
         return await self.websocket.recv()
