5a03a2c84f5d4cce607abcb0ffab0dd8cbbc5fd2
Caden Feller <caden.feller@gmail.com>
2024-02-29 14:28:16 -0800

    e

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import server
 turtles = []
 
 async def setTurtles(_turtles):
+    global turtles
     turtles = _turtles
 
+    
+    
+async def say():
     await turtles[0].turtle.send("return print('ea sports its in the game')")
