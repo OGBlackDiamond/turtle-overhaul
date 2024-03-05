beaef517706c492fc82026d0f34d68d8928b8ebb
OGBlackDiamond <caden.feller@gmail.com>
2024-03-05 10:31:45 -0800

    e

--- e.py
+++
@@ -1,6 +0,0 @@
-import main as e
-
-async def main():
-   await e.say()
-   
-main()
--- main.py
+++ main.py
@@ -5,8 +5,3 @@ turtles = []
 async def setTurtles(_turtles):
     global turtles
     turtles = _turtles
-
-    
-    
-async def say():
-    await turtles[0].turtle.send("return print('ea sports its in the game')")
